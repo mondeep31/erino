e90edd2b4e4253cf1bf1778bd27cf73f332d37ba
Mondeep Chetry <mandeepchetry6969@gmail.com>
2024-11-15 00:40:53 +0530

    fixed a small error

--- backend/src/services/contactService.ts
+++ backend/src/services/contactService.ts
@@ -1,89 +1,45 @@
-// import { Contact, PrismaClient } from "@prisma/client";
-
-// const prisma = new PrismaClient;
-
-// export const ContactService = {
-
-//     //create contact
-//     async createContact(data: Omit<Contact, 'id'>): Promise<Contact> {
-//         return prisma.contact.create({ data });
-//       },
-
-//     // get all contacts
-//     async getAllContacts(): Promise<Contact[]> {
-//         return prisma.contact.findMany();
-//     },
-
-//     //get contacts by id
-//     async getContactbyId(id: number): Promise<Contact | null> {
-//         return prisma.contact.findUnique({
-//             where: {
-//                 id
-//             },  
-//         })
-//     },
- 
-//     // delete contacts
-//     async deleteContact(id: number) : Promise<Contact | null>{
-//         return prisma.contact.delete({
-//             where: {
-//                 id
-//             }
-//         })
-//     },
-
-//     //update contact
-//     async updateContact(id: number, data: Partial<Contact>) : Promise<Contact | null >{
-//         return prisma.contact.update({
-//             where: {
-//                 id
-//             },
-//             data,
-//         })
-//     }
-
-// }
-
 import { Contact, PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient;
 
 export const ContactService = {
-  // create contact
-  async createContact(data: Omit<Contact, "id">): Promise<Contact> {
-    return prisma.contact.create({ data });
-  },
 
-  // get all contacts
-  async getAllContacts(): Promise<Contact[]> {
-    return prisma.contact.findMany();
-  },
-
-  // get contacts by id
-  async getContactbyId(id: number): Promise<Contact | null> {
-    return prisma.contact.findUnique({
-      where: {
-        id,
+    //create contact
+    async createContact(data: Omit<Contact, 'id'>): Promise<Contact> {
+        return prisma.contact.create({ data });
       },
-    });
-  },
 
-  // delete contacts
-  async deleteContact(id: number): Promise<Contact | null> {
-    return prisma.contact.delete({
-      where: {
-        id,
-      },
-    });
-  },
+    // get all contacts
+    async getAllContacts(): Promise<Contact[]> {
+        return prisma.contact.findMany();
+    },
 
-  // update contact
-  async updateContact(id: number, data: Partial<Contact>): Promise<Contact | null> {
-    return prisma.contact.update({
-      where: {
-        id,
-      },
-      data,
-    });
-  },
-};
+    //get contacts by id
+    async getContactbyId(id: number): Promise<Contact | null> {
+        return prisma.contact.findUnique({
+            where: {
+                id
+            },  
+        })
+    },
+ 
+    // delete contacts
+    async deleteContact(id: number) : Promise<Contact | null>{
+        return prisma.contact.delete({
+            where: {
+                id
+            }
+        })
+    },
+
+    //update contact
+    async updateContact(id: number, data: Partial<Contact>) : Promise<Contact | null >{
+        return prisma.contact.update({
+            where: {
+                id
+            },
+            data,
+        })
+    }
+
+}
